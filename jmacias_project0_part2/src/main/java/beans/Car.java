ae8399efbd9dec79756dbaea8217e3f5caa00fcd
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-07-08 19:50:41 -0400

    set up Car 2 param constructor (one used for adding new cars) to set number (id for car) to highest car id in db +1 (incrementing w/ java in stead of trigger in sql for now)

--- jmacias_project0_part2/src/main/java/beans/Car.java
+++ jmacias_project0_part2/src/main/java/beans/Car.java
@@ -1,8 +1,11 @@
 package beans;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+
+import dbControllers.DatabaseUtils;
 
 public class Car implements Serializable {
 	/**
@@ -10,13 +13,13 @@ public class Car implements Serializable {
 	 */
 	private static final long serialVersionUID = 3987990158017823409L;
 	
-	public static int count = 0; // use method in dbutils to start this at highest current count, so that numbering will be continuous within sessions
 	public int number; // TODO this is the id, will go through and change in all classes later
 	public String makeAndModel;
 	public int price;
 	public List<String> offers;
-	public Car(String makeAndModel, int price) {
+	public Car(String makeAndModel, int price) throws IOException {
 		super();
+		int count = DatabaseUtils.getMaxCarId();
 		count++;
 		this.number = count;
 		this.makeAndModel = makeAndModel;
